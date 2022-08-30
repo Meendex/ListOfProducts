045a6b164ed88cfb59be8bf97a76f3fd2cee3e1e
Mindaugas <mindaugas.balakauskas@gmail.com>
2022-08-30 21:58:37 +0100

    a few minor mistakes fix

--- ListOfProducts/ViewController.swift
+++ ListOfProducts/ViewController.swift
@@ -24,15 +24,13 @@ class ViewController: UIViewController, UITableViewDelegate {
     func createMockProducts()-> [Product] {
         let p1 = Product(productName:"Bananas", price:"£2.50", description:"FA banana is an elongated, edible fruit – botanically a berry[1][2] – produced by several kinds of large herbaceous flowering plants in the genus Musa.[3] In some countries, bananas used for cooking may be called plantains, distinguishing them from dessert", imageName:"Bananas")
         let p2 = Product(productName:"Apples", price:"£1.50", description:"Fair Trade", imageName:"Apples")
-
         let p3 = Product(productName:"Kiwi", price:"£3.25", description:"Fair Trade", imageName:"Kiwi")
-
         let p4 = Product(productName:"Strawberries", price:"£1.95", description:"Fair Trade", imageName:"Strawberries")
         let p5 = Product(productName:"Parslip", price:"£1.95", description:"Fair Trade", imageName:"Strawberries")
         let p6 = Product(productName:"Tomatoes", price:"£1.95", description:"Fair Trade", imageName:"Strawberries")
         let p7 = Product(productName:"Apricot", price:"£1.95", description:"Fair Trade", imageName:"Strawberries")
         let p8 = Product(productName:"Blackberries", price:"£1.95", description:"Fair Trade", imageName:"Strawberries")
-        let p9 = Product(productName:"Cheries", price:"£1.95", description:"Fair Trade", imageName:"Strawberries")
+        let p9 = Product(productName:"Cherries", price:"£1.95", description:"Fair Trade", imageName:"Strawberries")
         let p10 = Product(productName:"Cucumbear", price:"£1.95", description:"Fair Trade", imageName:"Strawberries")
 
        return [p1, p2, p3, p4, p5, p6, p7, p8, p9, p10]
